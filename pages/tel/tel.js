476ff65b95c35602740decc22fa022fa2d83ff9a
banpai <banpai@yeah.net>
2017-08-25 14:41:56 +0800

    start

---
+++ pages/tel/tel.js
@@ -0,0 +1,20 @@
+//index.js
+//获取应用实例
+var app = getApp()
+Page({
+  data: {
+    motto: 'Hello World',
+    userInfo: {}
+  },
+  //事件处理函数
+  bindViewTap: function() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  onLoad: function () {
+    wx.makePhoneCall({
+      phoneNumber: '1340000' //仅为示例，并非真实的电话号码
+    })
+  }
+})
